a4d6070af886ed4058c47b1e40a60240f46fa667
Yufeng Zhang <kinginsun@163.com>
2016-02-10 18:02:34 +0800

    Adds update action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     # We are going to list our resources here
     scope module: :v1,constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :update]
     end
   end
 end
